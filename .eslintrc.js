e2ea9bfe203f9eed82f400ce47a9638a7e75a71d
Swapnil Joshi <swapnil.a.joshi@accenture.com>
2020-02-06 17:19:55 +0100

    Correct eslint configurations for typescript checks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,25 @@
 module.exports = {
-    "parser": 'babel-eslint',
-    "env": {
-      "browser": true,
-      "commonjs": true,
-      "es6": true,
-      "node": true,
-      "jest": true,
+  extends: ["eslint:recommended", "plugin:react/recommended", 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
-    "extends": ["eslint:recommended", "plugin:react/recommended"],
-    "parserOptions": {
-      "ecmaFeatures": {
-        "experimentalObjectRestSpread": true,
-        "jsx": true
-      },
-      "sourceType": "module"
+    'import/resolver': {
+      typescript: {},
     },
-    "plugins": [ "react" ],
-    "rules": {
-      "react/prop-types": ["off"],
-      "indent": ["error", 2],
-      "linebreak-style": ["error","unix"],
-      "quotes": ["error","single"],
-      "no-console": ["warn", { "allow": ["info", "error"] }]
-    }
-  };
+  },
+  rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    '@typescript-eslint/indent': [2, 2],
+    'indent': ['error', 2],
+    'linebreak-style': ['error','unix'],
+    'quotes': ['error','single'],
+    'no-console': ['warn', { 'allow': ['info', 'error'] }]
+  },
+  globals: {
+        "console": false,
+        "document": false
+  }
+}
--- lib/components/App.tsx
+++ lib/components/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.PureComponent {
   state ={}
-  render() {
+  render(): ReactNode {
     return (<h2>This is where react app will run</h2>)
   }
 }
